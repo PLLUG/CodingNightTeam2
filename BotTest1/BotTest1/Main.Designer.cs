ad365e0b5e633e722df9d023d6ba073118542415
Dmitry Afanassyev <attflederx@gmail.com>
2017-05-13 00:52:12 +0300

    Working client buile w/ log

--- BotTest1/BotTest1/Main.Designer.cs
+++ BotTest1/BotTest1/Main.Designer.cs
@@ -28,21 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.tbTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // messageBox
+            // 
+            this.messageBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messageBox.Location = new System.Drawing.Point(13, 13);
+            this.messageBox.Multiline = true;
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(538, 261);
+            this.messageBox.TabIndex = 0;
+            // 
+            // tbTimer
+            // 
+            this.tbTimer.Tick += new System.EventHandler(this.tbTimer_Tick);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(563, 302);
+            this.Controls.Add(this.messageBox);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Name = "Main";
             this.Text = "BotTest";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        public System.Windows.Forms.TextBox messageBox;
+        private System.Windows.Forms.Timer tbTimer;
     }
 }
 
--- BotTest1/BotTest1/Main.cs
+++ BotTest1/BotTest1/Main.cs
@@ -7,14 +7,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Telegram.Bot;
+using ApiAiSDK;
+using ApiAiSDK.Model;
 
 namespace BotTest1
 {
     public partial class Main : Form
     {
+        string clientMsgLog = "";
+        string botMsgLog = "";
+        TelegramBotClient tbc;
+        ApiAi apiai;
+
         public Main()
         {
             InitializeComponent();
+            tbc = new TelegramBotClient("391813246:AAEDCf0OVWN0fZJjq-PIRZbM_6Q_7_7330s");
+            tbTimer.Start();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            var config = new AIConfiguration("90861bda9ce9466791b4b49321460248", SupportedLanguage.English);
+            apiai = new ApiAi(config);
+
+            tbc.OnMessage += Tbc_OnMessage;
+            tbc.StartReceiving();
+        }
+
+        private void Tbc_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            //if (e.Message != null)
+            //{
+            //    messageBox.Text += e.Message.Text + "\n";
+            //}
+            
+            clientMsgLog = e.Message.Chat.FirstName + ": " + e.Message.Text + "\r\n";
+
+            var response = apiai.TextRequest(e.Message.Text);
+            tbc.SendTextMessageAsync(e.Message.Chat.Id, response.Result.Fulfillment.Speech);
+            botMsgLog = "Bot: " + response.Result.Fulfillment.Speech + "\r\n";
+            //throw new NotImplementedException();
+        }
+
+        private void tbTimer_Tick(object sender, EventArgs e)
+        {
+            if (clientMsgLog.Length > 0)
+            {
+                messageBox.Text += clientMsgLog;
+                clientMsgLog = "";
+            }
+
+            if (botMsgLog.Length > 0)
+            {
+                messageBox.Text += botMsgLog;
+                botMsgLog = "";
+            }
         }
     }
 }
